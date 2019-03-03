ace01f1655be6ea0d278109d2d9e0e7d8ab44a38
Manoel Vilela <manoel_vilela@engineer.com>
2019-03-03 15:15:36 -0300

    Make docs.lisp more portable
    Pushing the current working directory to asdf:*central-registry* makes
    it more reliable to build the documentation if quicksys is not
    visible.

--- docs.lisp
+++ docs.lisp
@@ -1,6 +1,9 @@
-(ql:quickload :quicksys)
+(pushnew (uiop/os:getcwd) asdf:*central-registry*)
+(ql:register-local-projects)
+(ql:quickload :quicksys :silent t)
 (ql:quickload :staple :silent t)
 
 (staple:generate :quicksys
                  :packages '(#:quicksys))
+
 (sb-ext:exit :code 0)
